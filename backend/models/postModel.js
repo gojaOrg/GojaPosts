2a08045e9d996d84634c90f7ae2b108c44984509
Oscar Englöf <oscar.englof@hotmail.com>
2021-11-23 10:47:25 +0100

    audio file type

--- backend/models/postModel.js
+++ backend/models/postModel.js
@@ -14,6 +14,10 @@ const PostSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  audioFileType: {
+    type: String,
+    required: true,
+  },
   inReplyToUser: {
     type: mongoose.Schema.Types.ObjectId,
     default: null,
